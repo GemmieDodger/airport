3eb43fb73485b210e0e17e46573d95578ea822d6
gemmiedodger <gemmadruce@live.co.uk>
2020-09-25 22:20:45 +0100

    adding airpot and async tests

--- Airport.js
+++ Airport.js
@@ -1,10 +1,19 @@
+const fs = require('fs') //fs = file system, built into node
+const path = require('path')
+
 class Airport {
     static airports = []
 
-    constructor({name}) {
+    constructor(name) {
         this.name = name
         this.planes = []
         this.constructor.airports.push(this)
+
+        // {} THIS MEANS THE NAME BECOMES AN OBJECT AND REQUIRES US TO ASIGN AND OBJECT {name:'florida'}
+        // constructor({name}) {
+        //     this.name = name
+        //     this.planes = []
+        //     this.constructor.airports.push(this)
        
     }
     addPlane(plane){
@@ -13,13 +22,66 @@ class Airport {
     }
 
 
-    takeOff(Plane) {
+    takeOff(plane) {
         const index = this.planes.indexOf(plane)
         this.planes.splice(index,1)
         const destinationAirport = Airport.airports.find(airport =>airport.name === plane.destination) 
         console.log(destinationAirport)
         destinationAirport.addPlane(plane)
     }
-}
 
+    //ASYNC FUNCTIONS
+
+    //CALLBACK FUNCTION  - WORKING
+    // getInfo(onInfo) {
+    //     const airportName = this.name
+    //     const locationOfFile =path.join(__dirname,'airports.json')
+    //     const callback = function (err, buffer) {
+    //         const data = JSON.parse(String(buffer)) 
+    //         // const arrayOfAirports = Object.keys(data).map(key => { return data[key]})
+            
+    //         const info = data.find(airport => airport.iata === airportName)
+    //         console.log(airportName)
+    //         console.log(info)
+    //         onInfo(err,info)
+    //     }
+    //     fs.readFile(locationOfFile, callback)
+    // }
+    //A SYNC PROMISE FUNCTION - Working
+    getInfo() {
+        return new Promise((resolve, reject) => {
+            const locationOfFile =path.join(__dirname,'airports.json')
+            fs.readFile(locationOfFile, (err, data) => {
+                if (err) return reject(err)
+                const airportName = this.name //airport to be found
+                const dataString = JSON.parse(String(data))  //data of airports
+                const info = dataString.find(airport => airport.iata === airportName)
+                //CHECKS
+                // console.log(info)
+                // console.log(info.city)
+                resolve(info)
+            })
+        })
+    }
+   
+    // getInfo(onInfo) { //this doesn;t work yet
+    //     const airportName = this.name
+    //     const locationOfFile = path.join(__dirname, 'airports.json')
+    //     const callback = function (err, buffer) {
+    //         const data = JSON.parse(String(buffer))
+    //         const arrayOfAirports = Object.keys(data).map(key => )
+    //         console.log(Object.keys(data).filter())
+    //         onInfo(err,data)
+    //         fs.readFile(locationOfFile,callback)
+    // }
+    // getInfo() { //promise calls a function with resolve if right reject if wrong
+    //     return new Promise(function (resolve, reject) {
+    //         fs.readFile(path.join(__dirname,'airports.json'), (err, buffer) => {
+    //         const arrayOfAirports = Object.keys(data).map(key => )
+    //         console.log(Object.keys(data).filter())
+    //         onInfo(err,data)
+    //     })    
+
+
+}
 module.exports = Airport
--- airport.test.js
+++ airport.test.js
@@ -20,15 +20,68 @@ describe('Airport', function () {
     expect(Airport.airports.length).toBe(1)
     const tenerife = new Airport({name: "Tenerife"}) 
     expect(Airport.airports).toBeTruthy()
-    // expect(Airport.airports).toBe(2)
-  
+    console.log(Airport.airports)    
+    expect(Airport.airports.length).toBe(2)
  })   
 
- test('an airport has plane', () => {
-     const plane1 = new Plane({destination:"Florida"})
-     const [Heathrow, Dublin] = Airport.airports
-     Heathrow.addPlane(plane1)
-     expect(plane1.location).toBe('Heathrow')
+//  test('an airport has plane', () => {
+//      const plane1 = new Plane("Florida")
+//      const [Heathrow] = Airport.airports
+//      Heathrow.addPlane(plane1)
+//      console.log(plane1) 
+//      expect(plane1.location).toBe('Heathrow')
+//  })
 
- })
+
+
+// //CONDENSED TAKE OFF PROCEDURE
+
+   test('Check Gatwick to Lanzarote', () => {
+      
+      const airport1 = new Airport('Gatwick')
+      const airport2 = new Airport('Lanzarote')
+      const plane = new Plane({destination:'Lanzarote'})
+      // plane.setDestination(airport2.name)
+      airport1.addPlane(plane)
+      console.log(Airport.airports)
+      expect(plane.location).toEqual('Gatwick')
+      expect(plane.destination).toEqual('Lanzarote')
+      airport1.takeOff(plane)
+      expect(airport1.planes.length).toBe(0)
+      expect(airport2.planes.length).toBe(1)
+      expect(plane.location).toEqual('Lanzarote')
+      //console.log(Airport.airports)
+      })
+
+//ASYNC TESTS
+
+//ASYNC CALL BACKS
+
+      // test('airports have a city', (done) => {
+      //    const LHR = new Airport('LHR')
+      //    const onInfo = (err,info) => {
+      //       expect(info.city).toBe('London')
+      //       done()
+      //    }
+      //    LHR.getInfo(onInfo)     
+      // })
+
+//ASYNC PROMISE
+      // test('PROMISE airports have a city', () => {
+      //    const LHR = new Airport('LHR')
+      //    return LHR.getInfo()
+      //       .then(info => {
+      //          expect(info.city).toBe('London')
+      //       })
+      //       .catch(err => {
+      //           expect(err).toBeNull()
+      //       })
+      // })      
+      
+// I CAN'T REMEMBER WHAT THIS IS CALLED BUT IT WORKS!!!
+      test('have extra data', async () => {
+         const LHR = new Airport('LHR')
+         const info = await LHR.getInfo()
+         expect(info.city).toBe('London')
+   })
 })
